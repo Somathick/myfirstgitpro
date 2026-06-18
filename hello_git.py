04b67dda1506557d2f1afe89d826b23dc975f5eb
somathick <onyekwelusomadina@gmail.com>
2026-06-18 07:31:22 -0800

    updating my python files

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,5 @@
 print("Hello, World!")
 print("Welcome to Git version control.")
 print("Let's get started!")
+
+print("Why is it not working? There could be several reasons why the code is not working. Here are a few common issues to check")
