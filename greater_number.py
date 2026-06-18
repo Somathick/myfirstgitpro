0a9152158f0e1b678eea25401b8f6f2fdc966639
somathick <onyekwelusomadina@gmail.com>
2026-06-18 05:12:52 -0800

    initial commit

---
+++ greater_number.py
@@ -0,0 +1,6 @@
+num=int(input("please enter your first number"))
+num2=int(input("please enter your second number"))
+if num>num2:
+    print("the greater number is", num)
+else:
+    print("the greater number is", num2)
